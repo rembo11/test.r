3eb10b3606d423eafb208c8b51b17c2aa38bf92f
Rembo <122490345+rembo11@users.noreply.github.com>
2023-07-27 11:43:18 +0300

    Create game.tetris.c

---
+++ game.tetris.c
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void playTetris() {
+    // Code to play Tetris goes here
+    std::cout << "Playing Tetris..." << std::endl;
+}
+
+int main() {
+    // Call the function to play Tetris
+    playTetris();
+    
+    return 0;
+}
